b5de60980c7c536496056b26136aab37c2759551
Mike Reinstein <reinstein.mike@gmail.com>
2017-06-12 08:33:21 -0700

    0.1.1

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = {
   label: require('./lib/animate-label'),
   text: require('./lib/animate-text'),
   textLine: require('./lib/animate-text-line'),
-  timed: require('./lib/animate-text-timed-words')
+  timed: require('./lib/animate-text-timed-words'),
   words: require('./lib/animate-words')
 }
