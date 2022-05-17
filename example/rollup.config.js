e642174d1378f3c2a945fd319c32694888780965
Mike <reinstein.mike@gmail.com>
2022-05-17 00:19:06 -0700

    remove unused terser dep from example

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,12 +1,10 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve  from 'rollup-plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
 
 
 export default {
   plugins: [
     resolve(),
-    commonjs(),
-    //terser()
+    commonjs()
   ]
 }
